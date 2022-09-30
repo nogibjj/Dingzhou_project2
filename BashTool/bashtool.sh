b6aed6ec91db1b24a240e08124bff1c039fa7f03
DingzhouWang <wangdingzhou0918@gmail.com>
2022-09-30 18:56:08 -0400

    Update bashtool.sh

--- BashTool/bashtool.sh
+++ BashTool/bashtool.sh
@@ -1,12 +1,18 @@
-#!/usr/bin/env bash
+ #!/bin/bash
 
-#this a function that act as a place holder for now, which will be implemented later
-function count() {
-    echo "Hello developer!"
-    for (( i=1 ; i<=$1 ; i++ )); 
-    do
-        echo $i
-    done
-}
+while read line
+do
+  # read csv file
+  col1=$(echo ${line} | cut -d , -f 1)
+  col2=$(echo ${line} | cut -d , -f 2)
+  col3=$(echo ${line} | cut -d , -f 3)
+  col4=$(echo ${line} | cut -d , -f 4)
+  col5=$(echo ${line} | cut -d , -f 5)
+  col6=$(echo ${line} | cut -d , -f 6)
 
-count $1
+  # get data
+  echo "date:$col1 county:$col2 state:$col3 fips:$col4 cases:$col5 deaths:$col6" >> rec.txt
+done < $1
+
+# count how many cases in Illinois, Cook
+grep 2020-02 covid_data.csv | awk -F "," '{if($2=="Cook" && $3=="Illinois") sum+=$5} END {print sum}'  >> new.txt 
